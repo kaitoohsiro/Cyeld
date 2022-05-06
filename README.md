008bed5d5192b9efbab34d7eb3e6e2052405a6ab
kaitoohsiro <kaitooshiro7@gmail.com>
2022-05-07 04:52:50 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<div align="center">
+    <img src="./images/braint.png" alt="framework-picture">
+</div>
+
+<p></p>
+
+## About Cyeld
+
+> **Note:** This repository contains the core code of the Cyeld framework. If you want to build an application using Cyeld, visit the main .
+
+Cyeld is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable, creative experience to be truly fulfilling. Cyeld attempts to take the pain out of development by easing common tasks used in the majority of web projects, such as:
---
+++ cyeld/__init__.py
@@ -0,0 +1,43 @@
+# =============================================================================
+# step23.pyからstep32.pyまではsimple_coreを利用
+is_simple_core = False  # True
+# =============================================================================
+
+if is_simple_core:
+    from cyeld.core_simple import Variable
+    from cyeld.core_simple import Function
+    from cyeld.core_simple import using_config
+    from cyeld.core_simple import no_grad
+    from cyeld.core_simple import as_array
+    from cyeld.core_simple import as_variable
+    from cyeld.core_simple import setup_variable
+
+else:
+    from cyeld.core import Variable
+    from cyeld.core import Parameter
+    from cyeld.core import Function
+    from cyeld.core import using_config
+    from cyeld.core import no_grad
+    from cyeld.core import test_mode
+    from cyeld.core import as_array
+    from cyeld.core import as_variable
+    from cyeld.core import setup_variable
+    from cyeld.core import Config
+    from cyeld.layers import Layer
+    from cyeld.models import Model
+    from cyeld.datasets import Dataset
+    from cyeld.dataloaders import DataLoader
+    from cyeld.dataloaders import SeqDataLoader
+
+    import cyeld.datasets
+    import cyeld.dataloaders
+    import cyeld.optimizers
+    import cyeld.functions
+    import cyeld.functions_conv
+    import cyeld.layers
+    import cyeld.utils
+    import cyeld.cuda
+    import cyeld.transforms
+
+setup_variable()
+__version__ = '0.0.13'
